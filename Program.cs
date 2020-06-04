0c4d1b0f8e3314d9200d80e105c6e45a71ec6c76
tarytsa <a.tarytsa@gmail.com>
2020-06-04 15:55:07 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DivbyZero
+{
+    class Program
+    {
+        static double Div(int a,int b)
+        {
+            return Convert.ToDouble(a) / Convert.ToDouble(b);
+        }
+        static void Main(string[] args)
+        {
+            int a;
+            int b;
+            // int.TryParse(Console.ReadLine(), out a);
+            // int.TryParse(Console.ReadLine(), out b);
+            
+            try
+            {
+                a = Convert.ToInt32(Console.ReadLine());
+                b = Convert.ToInt32(Console.ReadLine());
+                double c = Div(a,b);
+                Console.WriteLine("{0:f1}",c);
+            }
+            catch(DivideByZeroException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
